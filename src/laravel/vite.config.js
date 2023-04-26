ffc3819ea5c4cb9d6fab87315b06b3c29c543c7c
tuna <ev0.kpx@gmail.com>
2023-04-26 09:56:58 +0900

    viteの設定を追加

--- src/laravel/vite.config.js
+++ src/laravel/vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'src-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
